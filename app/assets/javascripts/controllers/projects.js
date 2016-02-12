ee6268e9c77f3f8f2b52ee9e767b6caf2d3f2102
AloBin Technologies <support@alobin.com>
2016-02-12 12:50:51 +0000

    resolved the modal show in projects invite user modal

--- app/assets/javascripts/controllers/projects.js
+++ app/assets/javascripts/controllers/projects.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   //$("#inviteUserModal").modal();
   $(".invite-user-link").click(function() {
-    $("#inviteUserModal").open();
+    $("#inviteUserModal").modal('show');
   });
 });
