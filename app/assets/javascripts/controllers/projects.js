8df5fd288556389b76857d1b7657da13c93f2bdf
AloBin Technologies <support@alobin.com>
2016-02-13 12:17:02 +0000

    updated the code for attachments dialog display

--- app/assets/javascripts/controllers/projects.js
+++ app/assets/javascripts/controllers/projects.js
@@ -3,4 +3,18 @@ jQuery(document).ready(function($) {
   $(".invite-user-link").click(function() {
     $("#inviteUserModal").modal('show');
   });
+
+  $(".attachment .lightbox").click(function() {
+    var lightboxModal = $("#attachmentLightBoxModal");
+    var img = $(this).find('img');
+    if(img) {
+      var imgClone = $('<img src='+img.attr('src')+' width="100%" />');
+      lightboxModal.find('.lightbox-img').html(imgClone);
+      lightboxModal.find('.lightbox-file-title').text($(this).attr('data-file-name'));
+      lightboxModal.find('.lightbox-file-size').text($(this).attr('data-file-size'));
+      if($(this).attr('data-show-modal') != '') {
+        lightboxModal.modal('show');
+      }
+    }
+  });
 });
